092037114faafaf1fc1bd1255eb278f96163dd3c
kleeCollage <klee.collage@gmail.com>
2026-01-06 19:40:00 -0600

    feat: Decode and print in console data from API, Implement weatherData model

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Antonio Hernández Santander on 06/01/26.
+//  Copyright © 2026 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+}
--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -14,7 +14,7 @@ struct WeatherManager {
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)&appid=\(apiKey)"
-        print(urlString)
+        // print(urlString)
         perfomRequest(urlString: urlString)
     }
     
@@ -23,16 +23,34 @@ struct WeatherManager {
         if let url = URL(string: urlString) {
             // 2. Create a URLSession
             let session = URLSession(configuration: .default)
-    
             // 3. Give the session a task
-            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
-    
+            // let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
             // 4. Start the task
             task.resume()
         }
     }
     
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.main.temp)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
+        }
+        
+    }
+    /* func handle(data: Data?, response: URLResponse?, error: Error?) {
         if error != nil {
             print(error!)
             return 
@@ -42,6 +60,6 @@ struct WeatherManager {
             let dataString = String(data: safeData, encoding: .utf8)
             print(dataString!)
         }
-    }
+    } */
     
 }
